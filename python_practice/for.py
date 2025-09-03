1c46af68fc1c6bca486f2b0c463fbddd7feeacce
hardikkanodia <hardikkanodia1406@gmail.com>
2025-09-03 12:22:33 +0000

    Star

--- python_practice/for.py
+++ python_practice/for.py
@@ -1,1 +1,37 @@
-print("hello world")
+for i in range(5):
+    for j in range(15,i,-1):
+        print(" ", end=" ")
+    for j in range(i):
+        print("*", end="   ") 
+    print()      
+    
+
+for i in range(15,10,-1):
+    for j in range(15,i,-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end="   ")    
+    print() 
+
+
+
+for i in range(10,16):
+    for j in range(15,i,-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end="   ")    
+    print()
+
+
+for i in range(5,0,-1):
+    for j in range(15,i,-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end="   ")    
+    print()    
+
+    
+
+                   
+                
+
