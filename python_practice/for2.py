fae6caf05741a00cd300228198aaeaf1bdd472fb
hardikkanodia <hardikkanodia1406@gmail.com>
2025-09-03 12:39:25 +0000

    tree

---
+++ python_practice/for2.py
@@ -0,0 +1,32 @@
+for i in range(7):
+    for j in range(10,i,-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("-",end="   ")
+    print()
+
+for i in range(4,7):
+    for j in range(10,i,-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("-",end="   ")
+    print()
+
+for i in range(4,7):
+    for j in range(10,i,-1):
+        print(" ",end=" ")
+    for j in range(i):
+        print("-",end="   ")
+    print()    
+
+  
+
+for i in range(7):
+    for j in range(7):
+        print(" ",end=" ")
+    for j in range(3):
+        print("-",end="   ")
+    print()    
+
+
+
