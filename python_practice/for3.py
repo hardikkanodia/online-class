ee748b157391da0651a1803188ac4fd696f71316
hardikkanodia <hardikkanodia1406@gmail.com>
2025-09-08 11:47:15 +0000

    box

---
+++ python_practice/for3.py
@@ -0,0 +1,21 @@
+for i in range(10):
+    for j in range(9,i,-1):
+        print(" ",end = " ")
+    for j in range(i):
+        print(j,end = " ")
+    for j in range(i,-1,-1):
+        print(j,end = " ")
+    print()
+
+for i in range(8,1,-1):
+    for j in range(9,i,-1):
+        print(" ",end = " ")
+    for j in range(i):
+        print(j,end = " ")
+    for j in range(i,-1,-1):
+        print(j,end = " ")
+    print()    
+
+ 
+        
+
---
+++ python_practice/for4.py
@@ -0,0 +1,7 @@
+for i in range(9):
+    for j in range(9):
+        if(j>=2 and j<=6 and i>=2 and i<=6):
+            print(" ",end="  ")
+        else:
+            print("*",end="  ") 
+    print()           
