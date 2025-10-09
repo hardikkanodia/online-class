bd0d4399775910c2540a397e12d24cd475a230f0
hardikkanodia <hardikkanodia1406@gmail.com>
2025-10-09 12:03:36 +0000

    Read & Print of list

---
+++ python_practice/List1.py
@@ -0,0 +1,5 @@
+list1=["Car",7,-90,"Bus",'M']
+for i in range(6):
+  X=input("Enter the List:")
+  list1.append(X)
+print("Updated List:",list1)
---
+++ python_practice/lambda4.py
@@ -0,0 +1,8 @@
+Integer=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+Even_Num=list(filter(lambda x:x%2==0,Integer))
+print(Even_Num)
+
+Odd_Num=list(filter(lambda x:x%2!=0,Integer))
+print(Odd_Num)
+
